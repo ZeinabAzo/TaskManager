f98162835316ef339ce75841481a00a953de0bf6
Zeinab Azordeh <zeinab.azordeh@email.kntu.ac.ir>
2026-01-06 18:36:32 +0330

    Add search in manager

--- src/manager.py
+++ src/manager.py
@@ -1,15 +1,20 @@
-from .core import BTree, Task
+from .core import BTree, Task, intervalTree
 
 class TaskManager:
     def __init__(self):
-        self._tree = BTree(t=3)
+        self._Btree = BTree(t=3)
+        self.interval = intervalTree()
 
     def add_task(self, id, start, end, val, name):
         new_task = Task(id, start, end, val, name)
         self._tree.insert(new_task)
+        self.interval.add(new_task)
 
-    def get_task(self, task_id):
+    def get_task_id(self, task_id: int):
         return self._tree.search(task_id)
+    
+    def get_task_range(self, start_time: int, end_time: int):
+        return self.interval.search_overlap(start_time, end_time)
 
     def remove_task(self, task_id):
         self._tree.delete(task_id)
