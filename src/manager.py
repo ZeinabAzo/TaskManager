0cd1da1dab5fc6ab65dc01aed3e68d3ed1eac978
Zeinab Azordeh <zeinab.azordeh@email.kntu.ac.ir>
2026-01-28 18:10:08 +0330

    Add update def and logic in manager

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,5 @@
-from .core import BTree, Task, intervalTree
+from .core import BTree, intervalTree
+from .core.models import Task
 
 class TaskManager:
     def __init__(self):
@@ -6,6 +7,13 @@ class TaskManager:
         self.interval = intervalTree()
 
     def add_task(self, id, start, end, val):
+
+        overlaps : list[Task] = self.interval.search_overlap(start, end) # returns a list of tasks.
+        if overlaps:
+            for task in overlaps:
+                print(f"there is a conflict between {task.id} and {id}")
+            return
+        
         new_task = Task(id, start, end, val)
         self._Btree.insert(new_task)
         self.interval.add(new_task)
@@ -19,6 +27,11 @@ class TaskManager:
     def remove_task(self, task_id):
         self._Btree.delete(task_id)
         self.interval.delete(self.get_task_id(task_id)) #check for time complicity
+
+    def update_task(self, id, start, end, val):
+        self.remove_task(id)
+        self.add_task(id, start, end, val)
+
 
     def display_all(self):
         print("\n--- ⁂ Current Task Hierarchy (B-Tree) ⁂ ---")
